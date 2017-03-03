c52edd31ebc7497a7e621af80c16f91ce419e86f
AmirAliabadi <amiraliabadi@gmail.com>
2017-03-02 21:50:28 -0800

    removed the PWM code.  this was replace by PPM decoder.  Cannot get YYMFC to have a nice clean level angle reading from mpu.  off by +/- 1 degree

--- YMFC-AL_ppm_Flight_controller/YMFC-AL_ppm_Flight_controller.ino
+++ YMFC-AL_ppm_Flight_controller/YMFC-AL_ppm_Flight_controller.ino
@@ -245,14 +245,19 @@ void loop(){
   }
   
   //Place the MPU-6050 spirit level and note the values in the following two lines for calibration.
-  angle_pitch_acc -= 0.0;                                                   //Accelerometer calibration value for pitch.
-  angle_roll_acc -= 0.0;                                                    //Accelerometer calibration value for roll.
+  angle_pitch_acc -= 0.0; // -0.73; // (-0.90); // 0.0;                   //Accelerometer calibration value for pitch.
+  angle_roll_acc  -= 0.0; // -0.20; // (-0.05); // 0.0;                   //Accelerometer calibration value for roll.
+  //Serial.print( angle_pitch_acc );Serial.print( "\t" ); Serial.println( angle_roll_acc );
   
   angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;            //Correct the drift of the gyro pitch angle with the accelerometer pitch angle.
   angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;               //Correct the drift of the gyro roll angle with the accelerometer roll angle.
 
-  pitch_level_adjust = angle_pitch * 15;                                    //Calculate the pitch angle correction
-  roll_level_adjust = angle_roll * 15;                                      //Calculate the roll angle correction
+  //Serial.print( angle_pitch );Serial.print( "\t" ); Serial.println( angle_roll );  
+
+  pitch_level_adjust = (angle_pitch ) * 15;                                    //Calculate the pitch angle correction
+  roll_level_adjust =  (angle_roll  ) * 15;                                      //Calculate the roll angle correction
+
+  //Serial.print( pitch_level_adjust );Serial.print( "\t" ); Serial.println( roll_level_adjust );    
 
   if(!auto_level){                                                          //If the quadcopter is not in auto-level mode
     pitch_level_adjust = 0;                                                 //Set the pitch angle correction to zero.
@@ -334,8 +339,8 @@ void loop(){
 
   if( f_throttle < 1000 ) f_throttle = 1000;
   if( f_throttle > 2000 ) f_throttle = 2000;
-  if( throttle < 1000    ) throttle = 1000;
-  if( throttle > 2000    ) throttle = 2000;  
+  if( throttle < 1000   ) throttle = 1000;
+  if( throttle > 2000   ) throttle = 2000;  
   // AA
 
 
@@ -421,61 +426,6 @@ void loop(){
   digitalWrite(12, LOW);
 }
 
-/*
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//This routine is called every time input 8, 9, 10 or 11 changed state. This is used to read the receiver signals. 
-//More information about this subroutine can be found in this video:
-//https://youtu.be/bENjl1KQbvo
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ISR(PCINT0_vect){
-  current_time = micros();
-  //Channel 1=========================================
-  if(PINB & B00000001){                                                     //Is input 8 high?
-    if(last_channel_1 == 0){                                                //Input 8 changed from 0 to 1.
-      last_channel_1 = 1;                                                   //Remember current input state.
-      timer_1 = current_time;                                               //Set timer_1 to current_time.
-    }
-  }
-  else if(last_channel_1 == 1){                                             //Input 8 is not high and changed from 1 to 0.
-    last_channel_1 = 0;                                                     //Remember current input state.
-    receiver_input[1] = current_time - timer_1;                             //Channel 1 is current_time - timer_1.
-  }
-  //Channel 2=========================================
-  if(PINB & B00000010 ){                                                    //Is input 9 high?
-    if(last_channel_2 == 0){                                                //Input 9 changed from 0 to 1.
-      last_channel_2 = 1;                                                   //Remember current input state.
-      timer_2 = current_time;                                               //Set timer_2 to current_time.
-    }
-  }
-  else if(last_channel_2 == 1){                                             //Input 9 is not high and changed from 1 to 0.
-    last_channel_2 = 0;                                                     //Remember current input state.
-    receiver_input[2] = current_time - timer_2;                             //Channel 2 is current_time - timer_2.
-  }
-  //Channel 3=========================================
-  if(PINB & B00000100 ){                                                    //Is input 10 high?
-    if(last_channel_3 == 0){                                                //Input 10 changed from 0 to 1.
-      last_channel_3 = 1;                                                   //Remember current input state.
-      timer_3 = current_time;                                               //Set timer_3 to current_time.
-    }
-  }
-  else if(last_channel_3 == 1){                                             //Input 10 is not high and changed from 1 to 0.
-    last_channel_3 = 0;                                                     //Remember current input state.
-    receiver_input[3] = current_time - timer_3;                             //Channel 3 is current_time - timer_3.
-
-  }
-  //Channel 4=========================================
-  if(PINB & B00001000 ){                                                    //Is input 11 high?
-    if(last_channel_4 == 0){                                                //Input 11 changed from 0 to 1.
-      last_channel_4 = 1;                                                   //Remember current input state.
-      timer_4 = current_time;                                               //Set timer_4 to current_time.
-    }
-  }
-  else if(last_channel_4 == 1){                                             //Input 11 is not high and changed from 1 to 0.
-    last_channel_4 = 0;                                                     //Remember current input state.
-    receiver_input[4] = current_time - timer_4;                             //Channel 4 is current_time - timer_4.
-  }
-}
-*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Subroutine for reading the gyro
